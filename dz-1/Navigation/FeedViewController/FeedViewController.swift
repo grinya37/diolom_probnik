2dcc454608ee7849c5ac622b1e4ca5373880760e
grinya37 <112849355+grinya37@users.noreply.github.com>
2023-03-29 09:43:26 +0300

    add_project_AutoLayout

--- dz-1/Navigation/FeedViewController/FeedViewController.swift
+++ dz-1/Navigation/FeedViewController/FeedViewController.swift
@@ -9,40 +9,133 @@ import UIKit
 
 class FeedViewController: UIViewController {
   
-    var post = Post(title: "Мой пост")
+    private let  myPost = Post(title: "My post")
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .green
-        setupButton()
-    }
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        view.backgroundColor = .green
+//        //setupButton()
+//    }
     
-    private lazy var button: UIButton = {
-        let button = UIButton()
-        button.backgroundColor = .purple
-        button.layer.cornerRadius = 12
-        button.setTitle("Перейти на пост", for: .normal)
-        button.setTitleColor(.lightGray, for: .normal)
-        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 24)
-        button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+    private var topInsetView = UIView()
+    
+    private let stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.backgroundColor = .systemGray4
+        stackView.layer.cornerRadius = 4
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.spacing = 10
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+
+    private (set) lazy var showPostButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Show Post", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .systemRed
+        button.layer.shadowOffset = CGSize(width: 2, height: 2)
+        button.layer.cornerRadius = 4
+        button.layer.shadowColor = UIColor.black.cgColor
+        button.layer.shadowOpacity = 0.7
         button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(tapShowAction), for: .touchUpInside)
         return button
     }()
-    
-    private func setupButton() {
-        self.view.addSubview(self.button)
-        self.button.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -100).isActive = true
-        self.button.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 20).isActive = true
-        self.button.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -20).isActive = true
-        self.button.heightAnchor.constraint(equalToConstant: 50).isActive = true
+
+    private (set) lazy var editPostButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Edit Post", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .systemBlue
+        button.layer.shadowOffset = CGSize(width: 2, height: 2)
+        button.layer.cornerRadius = 4
+        button.layer.shadowColor = UIColor.black.cgColor
+        button.layer.shadowOpacity = 0.7
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(tapEditAction), for: .touchUpInside)
+        return button
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemIndigo
+        navigationItem.title = "Feed"
+        
+        topInsetView.backgroundColor = .white
+        view.addSubview(topInsetView)
+        
+        setupStackView()
+        setConstraints()
     }
     
-    @objc private func buttonAction() {
-        let postViewController = PostViewController()
-        self.navigationController?.pushViewController(postViewController, animated: true)
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        topInsetView.frame = CGRect(x: 0, y: 0,
+                                    width: view.frame.width,
+                                    height: view.safeAreaInsets.top)
+        
+    }
+
+    private func setupStackView() {
+        view.addSubview(stackView)
+        stackView.addArrangedSubview(showPostButton)
+        stackView.addArrangedSubview(editPostButton)
+    }
+
+    @objc func tapShowAction() {
+            let postVC = PostViewController(myPost: myPost.title)
+            navigationController?.pushViewController(postVC, animated: true)
+        }
+
+    @objc func tapEditAction() {
+            let postVC = PostViewController(myPost: myPost.title)
+            navigationController?.pushViewController(postVC, animated: true)
+        }
+
+}
+
+extension FeedViewController {
+
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10),
+            stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10),
+            stackView.heightAnchor.constraint(equalToConstant: 100)
+        ])
     }
 }
 
 
 
 
+
+//
+//
+//private lazy var button: UIButton = {
+//    let button = UIButton()
+//    button.backgroundColor = .purple
+//    button.layer.cornerRadius = 12
+//    button.setTitle("Перейти на пост", for: .normal)
+//    button.setTitleColor(.lightGray, for: .normal)
+//    button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 24)
+//    button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+//    button.translatesAutoresizingMaskIntoConstraints = false
+//    return button
+//}()
+//
+//private func setupButton() {
+//    self.view.addSubview(self.button)
+//    self.button.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -100).isActive = true
+//    self.button.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 20).isActive = true
+//    self.button.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -20).isActive = true
+//    self.button.heightAnchor.constraint(equalToConstant: 50).isActive = true
+//}
+//
+//@objc private func buttonAction() {
+//    let postViewController = PostViewController()
+//    self.navigationController?.pushViewController(postViewController, animated: true)
+//}
--- dz-1/Navigation/Profile/ProfileViewController.swift
+++ dz-1/Navigation/Profile/ProfileViewController.swift
@@ -7,33 +7,82 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {
+final class ProfileViewController: UIViewController {
     
-    var profileHeaderView = ProfileHeaderView()
+    
+    private let profileHeaderView: ProfileHeaderView = {
+        let profileHeaderView = ProfileHeaderView()
+        profileHeaderView.layer.cornerRadius = 5
+        return profileHeaderView
+    }()
+    
+    private var topInsetView = UIView()
+    
+    
+    private (set) lazy var showStatusButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Нажми меня", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .systemRed
+        button.layer.shadowOffset = CGSize(width: 2, height: 2)
+        button.layer.cornerRadius = 4
+        button.layer.shadowColor = UIColor.black.cgColor
+        button.layer.shadowOpacity = 0.7
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .lightGray
+        view.backgroundColor = .systemMint
+        setupViews()
+        setConstraints()
         navigationItem.title = "Profile"
-        //navigationController?.navigationBar.backgroundColor = .white
-        makeBarItems()
+        navigationController?.navigationBar.backgroundColor = .white
+        topInsetView.backgroundColor = .blue
+        view.addSubview(topInsetView)
+        
+        
     }
     
-    override func viewWillLayoutSubviews() {
-        super.viewWillLayoutSubviews()
-        profileHeaderView.frame = view.bounds
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        topInsetView.frame = CGRect(x: 0, y: 0,
+                                    width: view.frame.width,
+                                    height: view.safeAreaInsets.top)
+        
+    }
+    
+    private func setupViews() {
         view.addSubview(profileHeaderView)
-        profileHeaderView.backgroundColor = .lightGray
+        view.addSubview(showStatusButton)
     }
     
-    private func makeBarItems() {
-        let barItem = UIBarButtonItem(title: "👉", style: .plain, target: self, action: #selector(barItemAction))
-        navigationItem.rightBarButtonItem = barItem
+//        private func makeBarItems() {
+//            let barItem = UIBarButtonItem(title: "👉", style: .plain, target: self, action: #selector(barItemAction))
+//            navigationItem.rightBarButtonItem = barItem
+//        }
+//        @objc private func barItemAction() {
+//            let infoVC = InfoViewController()
+//            infoVC.title = "feedVC"
+//            infoVC.modalPresentationStyle = .fullScreen
+//            navigationController?.pushViewController(infoVC, animated: true)
+//        }
+        
+        private func setConstraints() {
+            profileHeaderView.translatesAutoresizingMaskIntoConstraints = false
+            showStatusButton.translatesAutoresizingMaskIntoConstraints = false
+            NSLayoutConstraint.activate([
+                profileHeaderView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+                profileHeaderView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 0),
+                profileHeaderView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 0),
+                profileHeaderView.heightAnchor.constraint(equalToConstant: 200),
+                
+                showStatusButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+                showStatusButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 0),
+                showStatusButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 0),
+                showStatusButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+                showStatusButton.heightAnchor.constraint(equalToConstant: 50)
+            ])
+        }
     }
-    @objc private func barItemAction() {
-        let infoVC = InfoViewController()
-        infoVC.title = "feedVC"
-        infoVC.modalPresentationStyle = .fullScreen
-        navigationController?.pushViewController(infoVC, animated: true)
-    }
-}
